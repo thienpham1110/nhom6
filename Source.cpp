2482d953de565601f35881f12e4051e35a24103b
thuan <liongentle@gmail.com>
2020-10-22 11:13:03 +0700

    viet ham tinh thuong + main

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,19 @@ float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a, int b;
+	cout<<"Nhập số a: ";
+	cin>>a;
+	cout<<"Nhập số b: ";
+	cin>>b;
+	cout<<"Tổng 2 số là: " + tong(a,b);
+	cout<<"Hiệu 2 số là: " + hieu(a,b);
+	cout<<"tích 2 số là: " + tich(a,b);
+	cout<<"Thương 2 số là: " + thuong(a,b);
 	system("pause");
 	return 0;
+}
+float thuong(int a, int b)
+{
+		return a/b;
 }
