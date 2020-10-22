dd72708eada99cf90bc161797c01c1acf0c119ef
unknown <thienthien111099@gmail.com>
2020-10-22 11:17:46 +0700

    tinh tong

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+
+int tong(int a, int b) {
+	int s = a + b;
+	return s;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a, b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout << "\nnhap a = ";
+	cin >> a;
+	cout << "\nnhap b = ";
+	cin >> b;
+	cout<<"tong = "<<tong(a, b)<<endl;
 	system("pause");
 	return 0;
 }
