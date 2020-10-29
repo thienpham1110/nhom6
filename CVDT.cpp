977d1ac5a2360eedba6edcfb1c98a45cf9229b62
thien <thienthien111099@gmail.com>
2020-10-29 11:24:28 +0700

    tinh chu vi 2

---
+++ CVDT.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+float chuViHV(int a){	
+	return a*4;
+}
+float chuViHCN(int a, int b){
+	return (a+b)*2;
+}
+float chuViTron(int r){
+	return r*2*3.14;
+}
+int main()
+{
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+
+	system("pause");
+	return 0;
+}
