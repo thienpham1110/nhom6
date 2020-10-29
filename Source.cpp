4ac57cb1cd228fa40972a3b0274ae1e320dfed7f
thien <thienthien111099@gmail.com>
2020-10-29 11:04:24 +0700

    phep toan co ban

--- Source.cpp
+++ Source.cpp
@@ -5,8 +5,12 @@ using namespace std;
 int tong(int a, int b){
 return(a+b);
 };
-int hieu(int a, int b);
-float thuong(int a, int b);
+int hieu(int a, int b){
+	return a-b;
+};
+float thuong(int a, int b){
+	return a/b;
+};
 int tich(int a, int b)
 {
 	
@@ -26,6 +30,8 @@ int main()
 	cin>>b;
 	cout<< "tong cua a va b la: "<<tong(a,b)<<endl;
 	cout<< "tich cua a va b la: "<<tich(a,b)<<endl;
+	cout<< "hieu cua a va b la: "<<hieu(a,b)<<endl;
+	cout<< "thuong cua a va b la: "<<thuong(a,b)<<endl;
 
 
 	system("pause");
