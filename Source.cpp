ca4b558bfd1e3216cdf302ddd8298c1ed092010d
thienpham1110 <thienthien111099@gmail.com>
2020-10-29 12:42:05 +0700

    merge branch update

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+float tich(int a, int b) {
+	return a*b;
+}
+float tong(int a, int b) {
+	return a+b;
+}
+float hieu(int a, int b) {
+	return a-b;
+}
 float thuong(int a, int b) {
 	return a/b;
+}
 int main()
 {
 
@@ -13,10 +23,10 @@ int main()
 	cin>>a;
 	cout<<"\nnhap b:";
 	cin>>b;
+	cout<< "tong cua a va b la: "<<tong(a,b)<<endl;
+	cout<< "hieu cua a va b la: "<<hieu(a,b)<<endl;
+	cout<< "tich cua a va b la: "<<tich(a,b)<<endl;
 	cout<< "thuong cua a va b la: "<<thuong(a,b)<<endl;
-
-
-
 	system("pause");
 	return 0;
 }
